9217c468e9fb025e1245750800101d3f3cd144ce
jyfernandez <jydfernandez@gmail.com>
2024-06-05 18:07:40 +0800

    Implement reservation service

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
 
 @Component({
   selector: 'app-reservation-form',
@@ -10,7 +12,9 @@ export class ReservationFormComponent implements OnInit{
   
   reservationForm: FormGroup = new FormGroup({});
   
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private reservationService: ReservationService) {
 
   }
 
@@ -26,7 +30,9 @@ export class ReservationFormComponent implements OnInit{
 
   onSubmit() {
     if(this.reservationForm.valid){
-      
+      let reservation: Reservation = this.reservationForm.value;
+      this.reservationService.addReservation(reservation);
     }
   }
+  
 }
